5aa6c91172683ae1c456cc038aa8bdef244486b6
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-03-26 22:42:02 +0600

    basic setup done and footer added

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+
+
+const Root = () => {
+    return (
+        <div>
+
+            <Outlet></Outlet>
+            <Footer></Footer>
+
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
