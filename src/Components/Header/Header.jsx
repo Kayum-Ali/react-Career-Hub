d1c83f037cea37bdd75a8c36edf0bef32c7639f4
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-03-26 23:59:05 +0600

    header update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-2xl font-bold">Career Hub</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -36,7 +36,7 @@ const Header = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">Apply Now</a>
             </div>
         </div>
     );
